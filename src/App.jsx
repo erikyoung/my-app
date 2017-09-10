5c72205466ecbea678a9d12abdbe122b77a572e8
erikyoung <erik.young@rmit.edu.vn>
2017-09-10 11:28:36 +0700

    setting the countdown time

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
 import './App.css';
+
 
 class App extends Component {
 	constructor(props) {
@@ -20,10 +22,7 @@ class App extends Component {
 		<div className="App">
 			<div className="App-title">{this.state.deadline}</div> 
 			<div>
-				<div className="Clock-days"> 14 Days</div>
-				<div className="Clock-hours"> 30 hours </div>
-				<div className="Clock-minutes"> 15 minutes </div> 
-				<div className="Clock-seconds"> 20 seconds </div> 
+				<Clock />
 			</div> 
 			<div>
 				<input 
